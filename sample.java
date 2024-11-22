9237aa573239e7273c00f1e861015d60e77fb694
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-11-22 11:36:35 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,15 +1,5 @@
-import java.util.Scanner; 
-
-public class SumOfTwoNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-        int sum = num1 + num2;
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
-        scanner.close();
+class Sample{
+    public static void main(String a[]){
+        System.out.println("hello wold");
     }
 }
-
