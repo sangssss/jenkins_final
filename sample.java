963f58b8a87fda28322dd0a4cbc8b37fb4ee57e7
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-11-22 12:26:03 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,18 @@
-class Sample{
-    public static void main(String a[]){
-        System.out.println("hello world");
+public class Calculator {
+
+    public static void main(String[] args) {
+        // Predefined values for addition and subtraction
+        int num1 = 10;
+        int num2 = 5;
+        int sum = add(num1, num2);
+        System.out.println("Addition of " + num1 + " and " + num2 + " is: " + sum);
+        int difference = subtract(num1, num2);
+        System.out.println("Subtraction of " + num1 + " and " + num2 + " is: " + difference);
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
